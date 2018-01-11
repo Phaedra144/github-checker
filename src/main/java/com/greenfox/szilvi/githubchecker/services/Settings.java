ced8039a3140d03c352646991740ddf48c532af5
Phaedra144 <bedi.szilvia@gmail.com>
2018-01-11 14:54:58 +0100

    add heroku url

--- src/main/java/com/greenfox/szilvi/githubchecker/services/Settings.java
+++ src/main/java/com/greenfox/szilvi/githubchecker/services/Settings.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class Settings {
 
     public static String GITHUB_TOKEN = "accessToken";
-    public static boolean IS_LOCALHOST = true;
+    public static boolean IS_LOCALHOST = false;
     public static String GITHUB_ORG = "greenfox-academy";
     public static int COHORT_ID = 2555784;
 
