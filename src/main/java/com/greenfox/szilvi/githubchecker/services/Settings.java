9fabb6cf0cb31179b6aee60842c2cc5daa91226f
Phaedra144 <bedi.szilvia@gmail.com>
2018-01-24 10:55:40 +0100

    switch back to localhost false

--- src/main/java/com/greenfox/szilvi/githubchecker/services/Settings.java
+++ src/main/java/com/greenfox/szilvi/githubchecker/services/Settings.java
@@ -3,6 +3,6 @@ package com.greenfox.szilvi.githubchecker.services;
 public class Settings {
 
     public static String GITHUB_TOKEN = "accessToken";
-    public static boolean IS_LOCALHOST = true;
+    public static boolean IS_LOCALHOST = false;
     public static String GITHUB_ORG = "greenfox-academy";
 }
