00949360f44744661cb486d48f9a033591c982b8
Phaedra144 <bedi.szilvia@gmail.com>
2018-07-03 19:49:44 +0200

    remove gfcommits tostring

--- src/main/java/com/greenfox/szilvi/githubchecker/commitcheck/model/GfCommits.java
+++ src/main/java/com/greenfox/szilvi/githubchecker/commitcheck/model/GfCommits.java
@@ -4,15 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class GfCommits {
-
     private String sha;
     private Commit commit;
 
-    @Override
-    public String toString() {
-        return "GfCommits{" +
-                "sha='" + sha + '\'' +
-                ", commit=" + commit +
-                '}';
-    }
 }
