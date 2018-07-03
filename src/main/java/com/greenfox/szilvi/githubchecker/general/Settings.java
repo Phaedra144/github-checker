b285c229d6aad91c39c29c350947abb6111ede3d
Phaedra144 <bedi.szilvia@gmail.com>
2018-07-03 14:13:32 +0200

    fix(): localhost false

--- src/main/java/com/greenfox/szilvi/githubchecker/general/Settings.java
+++ src/main/java/com/greenfox/szilvi/githubchecker/general/Settings.java
@@ -3,7 +3,7 @@ package com.greenfox.szilvi.githubchecker.general;
 public class Settings {
 
     public static String GITHUB_TOKEN = "accessToken";
-    public static boolean IS_LOCALHOST = true;
+    public static boolean IS_LOCALHOST = false;
     public static String GITHUB_ORG = "green-fox-academy";
     public static String TODO_APP = "todo-app";
     public static String WANDERER = "wanderer-";
