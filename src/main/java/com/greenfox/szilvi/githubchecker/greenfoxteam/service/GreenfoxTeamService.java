96b7bbc9905de6bd1cca41af6ff377cd590e07f1
Phaedra144 <bedi.szilvia@gmail.com>
2018-09-21 11:48:33 +0200

    fix(gfteamservice): not splitting one word into letters

--- src/main/java/com/greenfox/szilvi/githubchecker/greenfoxteam/service/GreenfoxTeamService.java
+++ src/main/java/com/greenfox/szilvi/githubchecker/greenfoxteam/service/GreenfoxTeamService.java
@@ -25,16 +25,17 @@ public class GreenfoxTeamService {
 
     public List<String> handleListOfHandles(String ghHandles){
         String splitter = "";
+        ArrayList<String> theHandles = new ArrayList<>();
         for(char strChar:ghHandles.toCharArray()){
             if(!splitter.contains(" ")){
                 if(!splitter.contains("\r") || !splitter.contains("\n")){
                     if (strChar == ' ' || strChar == '\r' || strChar == '\n'){
                         splitter = splitter + strChar;
+                        theHandles = new ArrayList<String>(Arrays.asList(ghHandles.split(splitter)));
                     }
                 }
             }
         }
-        ArrayList<String> theHandles = new ArrayList<String>(Arrays.asList(ghHandles.split(splitter)));
         return theHandles.size() > 1 ? theHandles : new ArrayList<>(Arrays.asList(ghHandles));
     }
 
