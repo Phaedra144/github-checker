9c3a4fc3fb76af0cc922bc436abc7e20361058d7
Phaedra144 <bedi.szilvia@gmail.com>
2018-10-08 17:03:22 +0200

    generation type to auto again

--- src/main/java/com/greenfox/szilvi/githubchecker/greenfoxteam/persistance/entity/ClassGithub.java
+++ src/main/java/com/greenfox/szilvi/githubchecker/greenfoxteam/persistance/entity/ClassGithub.java
@@ -17,7 +17,7 @@ import javax.persistence.Id;
 public class ClassGithub {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     long id;
     String cohortName;
     String className;
